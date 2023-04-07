ef276d2590f93a6bd32e9d2e3243cb878d4d10df
Bohdan Dovhal <46327995+H0us5@users.noreply.github.com>
2023-04-07 19:03:47 +0300

    adding portfolio section

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const rand = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+const title = document.querySelector(".portfolio__title--multicolor");
 
 const enhance = (id) => {
   const element = document.getElementById(id),
@@ -34,3 +35,19 @@ const enhance = (id) => {
 };
 
 enhance("channel-link");
+
+function randomColor() {
+  let color = [];
+  for (let i = 0; i < 3; i++) {
+    color.push(Math.floor(Math.random() * 256));
+  }
+  return "rgb(" + color.join(", ") + ")";
+}
+
+title.addEventListener("mouseover", function () {
+  title.style.color = randomColor();
+});
+
+title.addEventListener("mouseout", function () {
+  title.style.color = randomColor();
+});
