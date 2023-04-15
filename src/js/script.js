db47f1bebaec23ebe3ea51aeb3696149d826b2e3
Bohdan Dovhal <46327995+H0us5@users.noreply.github.com>
2023-04-15 22:04:57 +0300

    added track visits

--- src/js/script.js
+++ src/js/script.js
@@ -4,6 +4,50 @@ import "../img/image1.webp";
 const rand = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 const title = document.querySelector(".portfolio__title--multicolor");
 
+const trackVisits = () => {
+  let totalVisits = parseInt(localStorage.getItem("totalVisits")) || 0;
+  let visitsTime = parseInt(localStorage.getItem("visitsTime")) || 0;
+  const startTime = new Date().getTime();
+
+  window.addEventListener("beforeunload", function () {
+    const endTime = new Date().getTime();
+    const timeSpent = endTime - startTime;
+    visitsTime += timeSpent;
+    localStorage.setItem("visitsTime", parseInt(visitsTime));
+  });
+
+  const addVisit = () => {
+    totalVisits++;
+    localStorage.setItem("totalVisits", totalVisits);
+  };
+
+  addVisit();
+
+  const calcAvgVisitTime = () => {
+    const average = visitsTime / totalVisits;
+    const seconds = Math.floor(average / 1000);
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+    return { minutes, remainingSeconds };
+  };
+
+  let { minutes, remainingSeconds } = calcAvgVisitTime();
+
+  /* Була ще ідея збирати час відвідування у масив, і з нього вираховувати середнє значення. Так
+		воно було б наглядніше по часу сессій. Але розмір масиву збільшувався б постійно, 
+		що в перспективі могло б стати проблемою. Якщо на певному моменті 
+		почати видаляти старі значення, нові при цьому аншифтяться на початок, то вже не можна сказати, що
+		час середній за весь час. Було б лише за останні 50, 100 відвідувань. Залежить від обмеження. Тому 
+		вирішив записувати в змінну.
+	*/
+  console.log(`Вітаю! Ви зайшли на цей сайт вже ${totalVisits} разів`);
+  console.log(
+    `Ви провели на ньому в середньому ${minutes} хвилин ${remainingSeconds} секунд`
+  );
+};
+
+trackVisits();
+
 const enhance = (id) => {
   const element = document.getElementById(id),
     text = element.innerText.split("");
